1d60a75ca9a90b6d72d02d91699874cd75900f40
inonpeled <inon.peled@gmail.com>
2018-03-13 11:52:33 +0100

    get_murders.py

---
+++ lec6/python/get_murders.py
@@ -0,0 +1,34 @@
+import random
+import os
+import pandas as pd
+
+
+DATA_DIR = os.path.join('..', 'data')
+DATA_CSV_PATH = os.path.join(DATA_DIR, 'NYPD_Complaint_Data_Historic.csv')
+
+
+def get_murders(data_file_path, year):
+    df = pd.read_csv(
+        data_file_path,
+        usecols=['CMPLNT_FR_DT', 'CMPLNT_FR_TM',
+                 'KY_CD', 'OFNS_DESC', 'BORO_NM',
+                 'LOC_OF_OCCUR_DESC', 'PREM_TYP_DESC',
+                 'Latitude', 'Longitude'],
+        parse_dates=['CMPLNT_FR_DT', 'CMPLNT_FR_TM'])\
+        [lambda df: df.CMPLNT_FR_DT.dt.year == year]\
+        [lambda df: df.OFNS_DESC.str.contains('MURDER', na=False)]\
+        .assign(hour=lambda df: df.CMPLNT_FR_TM.str[:2].astype(int))
+    return df
+
+
+def sample(csv, out_csv, portion):
+    with open(csv) as in_f, open(out_csv, 'w') as out_f:
+        for i, line in enumerate(in_f):
+            if i == 0 or random.random() < portion:
+                out_f.write(line)
+
+
+if __name__ == '__main__':
+    # sample(DATA_CSV_PATH, os.path.join(DATA_DIR, 'sample.csv'), 0.0001)
+    get_murders(DATA_CSV_PATH, 2016)\
+        .to_csv(os.path.join(DATA_DIR, 'murders_2016.csv'))
