8cd92b0ea232405097eac7403f0f8b05baf3843e
Inon Peled <inon.peled@gmail.com>
2018-05-04 14:22:49 +0200

    WIP timeline as function

--- project/js/timeline_dropoffs.js
+++ project/js/timeline_dropoffs.js
@@ -1,17 +1,9 @@
-function plotTimeLineOfDropoffs(venueName) {
-// set the dimensions and margins of the graph
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-
-// parse the date / time
+function plotTimeLineOfDropoffs(margin, width, height, svgId, venueName) {
     var parseTime = d3.timeParse("%Y-%m-%d %H");
 
-// set the ranges
     var x = d3.scaleTime().range([0, width]);
     var y = d3.scaleLinear().range([height, 0]);
 
-// define the line
     var valueline = d3.line()
         .x(function (d) {
             return x(d.date);
@@ -20,17 +12,6 @@ function plotTimeLineOfDropoffs(venueName) {
             return y(d.dropoffs);
         });
 
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-    var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
-
-// Get the data
     d3.csv("./data/dropoffs_time_series_hourly.csv", function (error, data) {
         if (error) throw error;
 
@@ -51,18 +32,18 @@ function plotTimeLineOfDropoffs(venueName) {
         })]);
 
         // Add the valueline path.
-        svg.append("path")
+        d3.select("#" + svgId).select("#gPath")
             .data([data])
             .attr("class", "line")
             .attr("d", valueline);
 
         // Add the X Axis
-        svg.append("g")
+        d3.select("#" + svgId).select("#gX")
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(x));
 
         // Add the Y Axis
-        svg.append("g")
+        d3.select("#" + svgId).select("#gY")
             .call(d3.axisLeft(y));
 
     });
