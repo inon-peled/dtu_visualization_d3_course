7d649390583a4f7576169132db47b7f0748b36e5
Inon Peled <inon.peled@gmail.com>
2018-05-09 16:46:04 +0200

    Replace HTML slider with D3 slider

---
+++ project/js/slider.js
@@ -0,0 +1,44 @@
+function createSlider(svg, margin, width, height, cb) {
+    var x = d3.scaleLinear()
+        .domain([0, 5])
+        .range([0, width - 2 * margin])
+        .clamp(true);
+
+    var slider = svg.append("g")
+        .attr("class", "slider")
+        .attr("transform", "translate(" + margin + "," + (height / 2) + ")");
+
+    slider.append("line")
+        .attr("class", "track")
+        .attr("x1", x.range()[0])
+        .attr("x2", x.range()[1])
+        .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+        .attr("class", "track-inset")
+        .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+        .attr("class", "track-overlay")
+        .call(d3.drag()
+            .on("start.interrupt", function() { slider.interrupt(); })
+            .on("start drag", function() { updateHandle(x.invert(d3.event.x)); }));
+
+    slider.insert("g", ".track-overlay")
+        .attr("class", "ticks")
+        .attr("transform", "translate(0," + 18 + ")")
+        .selectAll("text")
+        .data(x.ticks(10))
+        .enter().append("text")
+        .attr("x", x)
+        .attr("text-anchor", "middle")
+        .text(function(d) { return d; });
+
+    var handle = slider.insert("circle", ".track-overlay")
+        .attr("class", "handle")
+        .attr("r", 9);
+
+    slider.transition()
+        .duration(750);
+
+    function updateHandle(h) {
+        handle.attr("cx", x(h));
+        cb(h);
+    }
+}
